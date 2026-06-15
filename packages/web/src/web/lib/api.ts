6c7956264908b348f3d765b709b2829c38b923e1
Ankur Verma <ankurv.ug23.cs@nitp.ac.in>
2026-06-15 07:16:21 +0530

    remove api logs

--- packages/web/src/web/lib/api.ts
+++ packages/web/src/web/lib/api.ts
@@ -3,7 +3,6 @@ import type { AppType } from "../../api";
 import { getToken } from "./auth";
 
 const API_URL = import.meta.env.VITE_API_URL;
-console.log("API_URL =", import.meta.env.VITE_API_URL);
 export const api = hc<AppType>(API_URL, {
   fetch: async (
     input: RequestInfo | URL,
